d1617365c2fdfb19e0b03323a657e1d807d847cd
mcantop <maciejcantop@icloud.com>
2022-08-28 14:45:54 +0200

    Initial Commit

---
+++ My Journal/My_JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_JournalApp.swift
+//  My Journal
+//
+//  Created by Maciej on 28/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct My_JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
